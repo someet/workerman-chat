f383c5113434acb738a37b4cf6bb9f6de3e6460c
boxplay <903199865@qq.com>
2020-01-09 17:32:30 +0800

    git登录

--- Applications/Chat/Config/Db.php
+++ Applications/Chat/Config/Db.php
@@ -5,9 +5,9 @@ class Db
     public static $chat = array(
 		'host' => 'rdso2xmawqj71m039c4u.mysql.rds.aliyuncs.com',
         'port' => 3306,
-        'user' => 'web',
-        'password' => 'web',
-        'dbname' => 'web',
+        'user' => 'sommet',
+        'password' => 'eddie221SOMEET',
+        'dbname' => 'someet',
         'charset' => 'utf8',
     );
 }
