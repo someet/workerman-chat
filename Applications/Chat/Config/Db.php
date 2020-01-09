e97e8024261f2bda9576dd81d9385727abba38c7
boxplay <903199865@qq.com>
2020-01-09 17:35:27 +0800

    workerman

--- Applications/Chat/Config/Db.php
+++ Applications/Chat/Config/Db.php
@@ -5,7 +5,7 @@ class Db
     public static $chat = array(
 		'host' => 'rdso2xmawqj71m039c4u.mysql.rds.aliyuncs.com',
         'port' => 3306,
-        'user' => 'sommet',
+        'user' => 'someet',
         'password' => 'eddie221SOMEET',
         'dbname' => 'someet',
         'charset' => 'utf8',
