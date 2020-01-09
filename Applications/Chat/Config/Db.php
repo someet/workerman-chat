23a271c057b39c207803e720a82ac3eed15be117
boxplay <903199865@qq.com>
2020-01-09 17:24:16 +0800

    增加权限

--- Applications/Chat/Config/Db.php
+++ Applications/Chat/Config/Db.php
@@ -3,8 +3,9 @@ namespace Config;
 class Db
 {
     public static $chat = array(
-        'host' => '100.98.73.241',
+        //'host' => '100.98.73.241',
         // 'host' => '123.56.179.91',
+	'host' => 'rdso2xmawqj71m039c4u.mysql.rds.aliyuncs.com',
         'port' => 3306,
         'user' => 'someet',
         'password' => 'eddie221SOMEET',
